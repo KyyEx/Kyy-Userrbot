7151fab31a225e1ae5d7593370ce5638d7f8c24b
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-02-12 15:38:18 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -19,7 +19,6 @@ from pySmartDL import SmartDL
 from pymongo import MongoClient
 from datetime import datetime
 from redis import StrictRedis
-from markdown import markdown
 from dotenv import load_dotenv
 from requests import get
 from telethon.sync import TelegramClient, custom, events
